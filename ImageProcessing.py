4d56b4d7da05c9e5b8ad9172389e41cf8b1af96b
islam <161100@ppu.edu.ps>
2020-11-22 19:09:27 +0200

    Computing a 10-bin histogram of an image (consider only grey level images)

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -25,3 +25,31 @@ def thumbnails_image(path):
         im.thumbnail((128,128))
         # im.show()
         im.save(file + "_thumbnail"+ ".PNG")
+# Computing a 10-bin histogram of an image (consider only grey level images)
+
+def Histogram_Computation(img):
+    ten_bins = np.zeros([10], np.int32)
+    size = img.size
+    Image_Height = size[0]
+    Image_Width = size[1]
+    sum = 0
+    Histogram = np.zeros([256], np.int32)
+    for x in range(0, Image_Height):
+        for y in range(0, Image_Width):
+            Histogram[img.getpixel((x, y))] += 1
+
+    for i in range(0, 256):
+        if (i % 25 != 0 or i == 0):
+            sum = sum + Histogram[i]
+        else:
+            index = i // 25
+            # print(index)
+            ten_bins[index - 1] = sum
+            sum = Histogram[i]
+
+        if (i == 255):
+            ten_bins[index - 1] = ten_bins[index - 1] + sum
+    # print(ten_bins)
+    # print(sum)
+    # print(Histogram)
+    return ten_bins
