998088b1f96a09bbb719ab358381436bf7609d3f
islam <161100@ppu.edu.ps>
2020-11-22 19:08:26 +0200

    Creating thumbnails of all files in a specific directory

--- ImageProcessing.py
+++ ImageProcessing.py
@@ -8,3 +8,20 @@ def gray_level_image(img):
     grey_img = img.convert("L")
     grey_img.show()
     return grey_img
+# Creating thumbnails of all files in a directory (current directory)
+def thumbnails_image(path):
+
+    # creating  thumbnail for (JPEG) image  in current directory
+    for infile in glob.glob(path+"/*.jpg"):
+        file, ext = os.path.splitext(infile)
+        im = Image.open(infile)
+        im.thumbnail((128,128))
+        # im.show()
+        im.save(file + "_thumbnail"+ ".JPEG")
+    # creating  thumbnail for (PNG) image  in current directory
+    for infile in glob.glob(path+"/*.png"):
+        file, ext = os.path.splitext(infile)
+        im = Image.open(infile)
+        im.thumbnail((128,128))
+        # im.show()
+        im.save(file + "_thumbnail"+ ".PNG")
